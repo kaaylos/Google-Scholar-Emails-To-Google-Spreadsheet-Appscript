78c6105c1d43c49861a738458fc6f0d51da82ec6
Tom Grey <35573671+kaaylos@users.noreply.github.com>
2026-01-19 20:17:10 +0000

    Handle multiple messages per thread
    Updated code to handle the situation where there is more than one message in a thread (seems to happen occasionally!)

--- script.js
+++ script.js
@@ -11,34 +11,38 @@ function processEmails() {
   Logger.log("Got "+threads.length+" threads to process");
   for (thread of threads)
   {
-    msg = thread.getMessages()[0]
-    htmlstr = msg.getBody();
-    dateSent = msg.getDate();
-    subject = msg.getSubject();
-    rows = htmlstr.split("<a href=\"");
-    for (row of rows)
+    msgs = thread.getMessages()
+    Logger.log("Got "+msgs.length+" messages to process");
+    for (msg of msgs)
     {
-      if (row.startsWith("https://scholar.google.com/scholar_url")) //Splits body into chunks starting with each link and then only keeps those that are scholar links
+      htmlstr = msg.getBody();
+      dateSent = msg.getDate();
+      subject = msg.getSubject();
+      rows = htmlstr.split("<a href=\"");
+      for (row of rows)
       {
-        anchor = row.split("</a>")[0]; //Crappy way to throw away the text after the anchor
-        anchortokens = anchor.split("\""); //Split it on a double quote
-        url = anchortokens[0].replaceAll("&amp;","&"); //And the first chunk is the href, but need to reverse html encoding of amp
-        title = anchortokens[anchortokens.length-1].replaceAll(/<\/?[^>]+(>|$)/g, ""); //Last chunk is the text of the link, which is the title, then strip out the html charactures
-        data.push([dateSent, subject, url, title]);
+        if (row.startsWith("https://scholar.google.com/scholar_url")) //Splits body into chunks starting with each link and then only keeps those that are scholar links
+        {
+          anchor = row.split("</a>")[0]; //Crappy way to throw away the text after the anchor
+          anchortokens = anchor.split("\""); //Split it on a double quote
+          url = anchortokens[0].replaceAll("&amp;","&"); //And the first chunk is the href, but need to reverse html encoding of amp
+          title = anchortokens[anchortokens.length-1].replaceAll(/<\/?[^>]+(>|$)/g, ""); //Last chunk is the text of the link, which is the title, then strip out the html charactures
+          data.push([dateSent, subject, url, title]);
+        }
       }
-    }
-    if (data.length == 0)
-    {
-      throw new Error("Got matching email but no links... The parser is probably broken...");
-    }
+      if (data.length == 0)
+      {
+        throw new Error("Got matching email but no links... The parser is probably broken...");
+      }
+    } //Loop over messages
     thread.addLabel(label);
-  }
+  } //Loop over threads
   return data;
 }
 
 function appendToSpreadsheet(data)
 {
-  var sheet= SpreadsheetApp.openById('<PUT YOUR SPREADSHEET ID HERE>').getSheetByName('Sheet1');
+  var sheet= SpreadsheetApp.openById('1gdQcfoKH9NH5zWK1STzZ5YDzQuHWqIF1UXC7M_JpO54').getSheetByName('Sheet1');
   for (row of data)
   {
     sheet.appendRow(row);
